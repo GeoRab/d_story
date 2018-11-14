b0f9cf864e17dc9e71a12eaddc2b7883ae369e13
BEUTNER <beutner@rstudiosrv.hws.edu>
2018-11-13 23:48:43 -0500

    Blog Start up - Commit 1

---
+++ Start.R
@@ -0,0 +1,7 @@
+#reference:https://www.youtube.com/watch?v=rFr1OmS7V9Y&t=178s
+
+#establishes a new site in a directory
+blogdown::new_site(theme = "gcushen/hugo-academic")
+
+#starts the site
+blogdown::serve_site()
